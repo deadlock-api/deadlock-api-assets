5871c683a7102036f2093efca842bd5f923d23ba
Manuel Raimann <raimannma@outlook.de>
2025-03-04 09:54:31 +0100

    Add localization token override for raw_property

--- deadlock_assets_api/models/v2/api_item_base.py
+++ deadlock_assets_api/models/v2/api_item_base.py
@@ -50,6 +50,7 @@ class ItemPropertyV2(RawItemPropertyV2):
         key: str,
         localization: dict[str, str],
     ) -> dict:
+        key = raw_property.get("loc_token_override", key)
         raw_property["icon"] = parse_img_path(raw_property["icon_path"])
         del raw_property["icon_path"]
         if key == "BuildUpDuration":
