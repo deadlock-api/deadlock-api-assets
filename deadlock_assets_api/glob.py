6e1f59bcd350b8223101eb474cc3ffbffbd189a7
Manuel Raimann <raimannma@outlook.de>
2025-04-08 21:55:44 +0200

    Fix

--- deadlock_assets_api/glob.py
+++ deadlock_assets_api/glob.py
@@ -1,4 +1,4 @@
 IMAGE_BASE_URL = "https://assets-bucket.deadlock-api.com/assets-api-res/images"
-SVGS_BASE_URL = "https://assets-bucket.deadlock-api.com/assets-api-res/svgs"
+SVGS_BASE_URL = "https://assets-bucket.deadlock-api.com/assets-api-res/icons"
 SOUNDS_BASE_URL = "https://assets-bucket.deadlock-api.com/assets-api-res"
 VIDEO_BASE_URL = "https://assets-bucket.deadlock-api.com/assets-api-res/videos"
