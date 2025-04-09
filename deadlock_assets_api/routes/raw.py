38cbff5629d283abc41c691a596afa159a153b53
Manuel Raimann <raimannma@outlook.de>
2025-04-09 12:33:07 +0200

    Rename raw endpoint methods

--- deadlock_assets_api/routes/raw.py
+++ deadlock_assets_api/routes/raw.py
@@ -5,18 +5,18 @@ router = APIRouter(prefix="/raw", tags=["Raw"])
 
 
 @router.get("/heroes")
-def get_heroes() -> FileResponse:
+def get_raw_heroes() -> FileResponse:
     return FileResponse("res/raw_heroes.json")
+
+
+@router.get("/items")
+def get_raw_items() -> FileResponse:
+    return FileResponse("res/raw_items.json")
 
 
 @router.get("/generic_data")
 def get_generic_data() -> FileResponse:
     return FileResponse("res/raw_generic_data.json")
-
-
-@router.get("/items")
-def get_items() -> FileResponse:
-    return FileResponse("res/raw_items.json")
 
 
 @router.get("/colors")
