46f829d28918db741f4803c2ee20b6f82a64bbff
Manuel Raimann <raimannma@outlook.de>
2025-05-10 15:55:59 +0200

    Add SVG processing to include currentColor-filled variants
    This update extends the script to generate additional SVGs with `currentColor` fills, excluding those already marked as "_unfilled". This ensures better support for theming and styling in downstream usage.

--- extract_game_files.sh
+++ extract_game_files.sh
@@ -109,6 +109,15 @@ find depots/game/ -type f -name '*.svg' -print0 | xargs -0 -n 1 cp -t svgs/
 find depots/game/ -type f -name 'keystat_*.png' -print0 | xargs -0 -n 1 cp -t svgs/
 find svgs -type f -name "*_png.*" -exec bash -c 'mv "$1" "${1/_png./.}"' _ {} \;
 
+# Add SVGs with currentColor fill
+for f in svgs/*.svg;
+do
+    if [[ "$f" == *"_unfilled.svg" ]]; then
+        continue
+    fi
+    sed 's/fill="[^"]*"/fill="currentColor"/g' "$f" > "${f%.svg}_unfilled.svg";
+done
+
 # Extract css files
 cp "$citadel_folder"/panorama/styles/ability_icons.css res/
 cp "$citadel_folder"/panorama/styles/ability_properties.css res/
