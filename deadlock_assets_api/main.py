07bc6a65d2798ebbb2b52537c894a678c2c53311
Manuel Raimann <raimannma@outlook.de>
2025-11-08 15:58:02 +0100

    chore: handle preflight OPTIONS requests in CORS middleware

--- deadlock_assets_api/main.py
+++ deadlock_assets_api/main.py
@@ -67,6 +67,15 @@ app.add_middleware(RouterLoggingMiddleware, logger=LOGGER)
 
 @app.middleware("http")
 async def cors_handler(request: Request, call_next):
+    # Handle preflight OPTIONS requests
+    if request.method == "OPTIONS":
+        response = Response(status_code=200)
+        response.headers["Access-Control-Allow-Credentials"] = "true"
+        response.headers["Access-Control-Allow-Origin"] = "*"
+        response.headers["Access-Control-Allow-Methods"] = "*"
+        response.headers["Access-Control-Allow-Headers"] = "*"
+        return response
+
     response: Response = await call_next(request)
     response.headers["Access-Control-Allow-Credentials"] = "true"
     response.headers["Access-Control-Allow-Origin"] = "*"
