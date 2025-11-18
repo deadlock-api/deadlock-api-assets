d8302c3202434db361b0d8dc67f3386d16b63a2e
Manuel Raimann <raimannma@outlook.de>
2025-11-18 16:54:52 +0100

    feat: add additional fields for spawn delays and loot list in misc.py

--- deadlock_assets_api/models/v2/misc.py
+++ deadlock_assets_api/models/v2/misc.py
@@ -26,6 +26,19 @@ class MiscV2(BaseModel):
     initial_spawn_time: float | None = Field(None, validation_alias="m_flInitialSpawnTime")
     respawn_time: float | None = Field(None, validation_alias="m_flRespawnTime")
     spawn_interval: float | None = Field(None, validation_alias="m_flSpawnInterval")
+    initial_spawn_delay_in_seconds: int | None = Field(
+        None, validation_alias="m_iInitialSpawnDelayInSeconds"
+    )
+    spawn_interval_in_seconds: int | None = Field(
+        None, validation_alias="m_iSpawnIntervalInSeconds"
+    )
+    match_time_mins_for_level2_pickups: int | None = Field(
+        None, validation_alias="m_iMatchTimeMinsForLevel2Pickups"
+    )
+    match_time_mins_for_level3_pickups: int | None = Field(
+        None, validation_alias="m_iMatchTimeMinsForLevel3Pickups"
+    )
+    loot_list_deck_size: int | None = Field(None, validation_alias="m_iLootListDeckSize")
     initial_spawn_delay_seconds: int | None = Field(
         None, validation_alias="m_iInitialSpawnDelayInSeconds"
     )
@@ -45,6 +58,12 @@ class MiscV2(BaseModel):
     primary_pickups: list[PickupDefinition] | None = Field(
         None, validation_alias="m_vecPrimaryPickups"
     )
+    m_vecPickups_lv2: list[PickupDefinition] | None = Field(
+        None, validation_alias="m_vecPickups_lv2"
+    )
+    m_vecPickups_lv3: list[PickupDefinition] | None = Field(
+        None, validation_alias="m_vecPickups_lv3"
+    )
     roll_type: str | None = Field(None, validation_alias="m_eRollType")
 
     # Pickup/Powerup Specifics
@@ -52,11 +71,6 @@ class MiscV2(BaseModel):
     expiration_duration: float | None = Field(None, validation_alias="m_flPickupExpirationDuration")
     show_on_minimap: bool | None = Field(None, validation_alias="m_bShowOnMinimap")
     name_loc_string: str | None = Field(None, validation_alias="m_sNameLocString")
-
-    # Audio
-    break_sound: str | None = Field(None, validation_alias="m_sBreakSound")
-    spawn_sound: str | None = Field(None, validation_alias="m_sSpawnSound")
-    pickup_sound: str | None = Field(None, validation_alias="m_sPickupSound")
 
     # XP Orb Specifics
     orb_spawn_delay_min: float | None = Field(None, validation_alias="m_flOrbSpawnDelayMin")
