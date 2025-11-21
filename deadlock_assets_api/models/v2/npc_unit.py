23f56b7f9f0025f1b7abddfa8133d7d70645e809
Manuel Raimann <raimannma@outlook.de>
2025-11-21 10:58:55 +0100

    feat: update NPCUnitV2 to use ColorV1 and add color validation

--- deadlock_assets_api/models/v2/npc_unit.py
+++ deadlock_assets_api/models/v2/npc_unit.py
@@ -2,13 +2,12 @@ import logging
 from typing import Annotated
 
 from murmurhash2 import murmurhash2
-from pydantic import ConfigDict, Field, BaseModel, computed_field, WithJsonSchema
+from pydantic import ConfigDict, Field, BaseModel, computed_field, WithJsonSchema, field_validator
 
+from deadlock_assets_api.models.v1.colors import ColorV1
 from deadlock_assets_api.models.v2.raw_weapon import RawWeaponInfoV2
 
 LOGGER = logging.getLogger(__name__)
-
-Color = tuple[int, int, int] | tuple[int, int, int, int]
 
 
 class NPCUnitV2(BaseModel):
@@ -78,20 +77,80 @@ class NPCUnitV2(BaseModel):
     barrack_boss_dps: float | None = Field(None, validation_alias="m_flBarrackBossDPS")
     player_dps: float | None = Field(None, validation_alias="m_flPlayerDPS")
     trooper_dps: float | None = Field(None, validation_alias="m_flTrooperDPS")
-    health_bar_color_friend: Color | None = Field(None, validation_alias="m_HealthBarColorFriend")
-    health_bar_color_enemy: Color | None = Field(None, validation_alias="m_HealthBarColorEnemy")
-    health_bar_color_team1: Color | None = Field(None, validation_alias="m_HealthBarColorTeam1")
-    health_bar_color_team2: Color | None = Field(None, validation_alias="m_HealthBarColorTeam2")
-    health_bar_color_team_neutral: Color | None = Field(
+    health_bar_color_friend: ColorV1 | None = Field(None, validation_alias="m_HealthBarColorFriend")
+    health_bar_color_enemy: ColorV1 | None = Field(None, validation_alias="m_HealthBarColorEnemy")
+    health_bar_color_team1: ColorV1 | None = Field(None, validation_alias="m_HealthBarColorTeam1")
+    health_bar_color_team2: ColorV1 | None = Field(None, validation_alias="m_HealthBarColorTeam2")
+    health_bar_color_team_neutral: ColorV1 | None = Field(
         None, validation_alias="m_HealthBarColorTeamNeutral"
     )
-    glow_color_friend: Color | None = Field(None, validation_alias="m_GlowColorFriend")
-    glow_color_enemy: Color | None = Field(None, validation_alias="m_GlowColorEnemy")
-    glow_color_team1: Color | None = Field(None, validation_alias="m_GlowColorTeam1")
-    glow_color_team2: Color | None = Field(None, validation_alias="m_GlowColorTeam2")
-    glow_color_team_neutral: Color | None = Field(None, validation_alias="m_GlowColorTeamNeutral")
+    glow_color_friend: ColorV1 | None = Field(
+        None,
+        validation_alias="m_GlowColorFriend",
+    )
+    glow_color_enemy: ColorV1 | None = Field(
+        None,
+        validation_alias="m_GlowColorEnemy",
+    )
+    glow_color_team1: ColorV1 | None = Field(
+        None,
+        validation_alias="m_GlowColorTeam1",
+    )
+    glow_color_team2: ColorV1 | None = Field(
+        None,
+        validation_alias="m_GlowColorTeam2",
+    )
+    glow_color_team_neutral: ColorV1 | None = Field(
+        None,
+        validation_alias="m_GlowColorTeamNeutral",
+    )
 
     @computed_field
     @property
     def id(self) -> Annotated[int, WithJsonSchema({"format": "int64", "type": "integer"})]:
         return murmurhash2(self.class_name.encode(), 0x31415926)
+
+    @field_validator(
+        "health_bar_color_friend",
+        "health_bar_color_enemy",
+        "health_bar_color_team1",
+        "health_bar_color_team2",
+        "health_bar_color_team_neutral",
+        "glow_color_friend",
+        "glow_color_enemy",
+        "glow_color_team1",
+        "glow_color_team2",
+        "glow_color_team_neutral",
+        mode="before",
+    )
+    @classmethod
+    def validate_colors(cls, v: ColorV1 | list[int] | None | dict[str, int]) -> ColorV1 | None:
+        if v is None:
+            return v
+        if isinstance(v, ColorV1):
+            return v
+        if isinstance(v, dict):
+            try:
+                return ColorV1.model_validate(v)
+            except ValueError as e:
+                LOGGER.error(f"Error parsing color {v}: {e}")
+                raise
+        if isinstance(v, list):
+            try:
+                color = parse_color(v)
+                return color
+            except ValueError as e:
+                LOGGER.error(f"Error parsing color {v}: {e}")
+                raise
+        raise TypeError(f"Invalid type for color field: {type(v)}")
+
+
+def parse_color(color: list[int]) -> ColorV1:
+    if len(color) == 3:
+        r, g, b = color
+        a = 255
+    elif len(color) == 4:
+        r, g, b, a = color
+    else:
+        raise ValueError("Color must be a tuple or list of 3 or 4 integers.")
+    return ColorV1(red=r, green=g, blue=b, alpha=a)
