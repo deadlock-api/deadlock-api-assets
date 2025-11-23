f2c401477e77085710f27da718fa377155a3f14a
Manuel Raimann <raimannma@outlook.de>
2025-11-23 15:00:22 +0100

    do not filter out base npc units

--- deadlock_assets_api/parsers/npc_units.py
+++ deadlock_assets_api/parsers/npc_units.py
@@ -6,9 +6,4 @@ LOGGER = logging.getLogger(__name__)
 
 
 def parse_npc_units_v2(data: dict) -> list[NPCUnitV2]:
-    npc_dicts = {
-        k: v
-        for k, v in data.items()
-        if "base" not in k and "dummy" not in k and isinstance(v, dict)
-    }
-    return [NPCUnitV2(class_name=k, **v) for k, v in npc_dicts.items()]
+    return [NPCUnitV2(class_name=k, **v) for k, v in data.items() if isinstance(v, dict)]
