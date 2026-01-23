c43f780270d13e9919b4b42625727c5e109c6f4d
Manuel Raimann <raimannma@outlook.com>
2026-01-23 14:53:49 +0100

    fix: exclude keys starting with 'generic_' from loot table parsing

--- deadlock_assets_api/parsers/loot_table.py
+++ deadlock_assets_api/parsers/loot_table.py
@@ -2,4 +2,6 @@ from deadlock_assets_api.models.v2.loot_table import LootTablesV2
 
 
 def parse_loot_table(data: dict) -> LootTablesV2:
-    return LootTablesV2(**{k: v for k, v in data.items() if k != "all_items"})
+    return LootTablesV2(
+        **{k: v for k, v in data.items() if k != "all_items" and not k.startswith("generic_")}
+    )
