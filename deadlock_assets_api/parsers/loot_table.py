a5b6bac733c190194edb6debde8c65aa951b003c
Manuel Raimann <raimannma@outlook.com>
2026-01-23 14:52:43 +0100

    fix: exclude 'all_items' key from loot table parsing

--- deadlock_assets_api/parsers/loot_table.py
+++ deadlock_assets_api/parsers/loot_table.py
@@ -2,4 +2,4 @@ from deadlock_assets_api.models.v2.loot_table import LootTablesV2
 
 
 def parse_loot_table(data: dict) -> LootTablesV2:
-    return LootTablesV2(**data)
+    return LootTablesV2(**{k: v for k, v in data.items() if k != "all_items"})
